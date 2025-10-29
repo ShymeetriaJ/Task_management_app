53e4164c07cedef7293d4ce83d4731847d9c994a
ShymeetriaJ <sjeducates2@gmail.com>
2025-10-29 14:00:38 -0400

    Added const functions.

---
+++ app.js
@@ -0,0 +1,27 @@
+const textBox = document.getElementById
+('textbox');
+const taskName = document.getElementById
+('task-name');
+const categoryInput = document.getElementById
+('category');
+const deadLineInput = document.getElementById
+('deadLine');
+const statusInput = document.getElementById
+('status');
+const addTaskBtn = document.getElementById
+('btn1');
+
+    const addTask = (event) => {
+        const taskText = task-name.value.trim
+    }
+
+ 
+        
+
+
+// const addTask = (event) => {
+//     const taskText = textbox.value.trim();
+// }
+
+
+
