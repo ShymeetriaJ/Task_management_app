e3ccda7571428c1d8ad0f110260dacf653621678
ShymeetriaJ <sjeducates2@gmail.com>
2025-10-29 16:14:50 -0400

    Added an array for daily tasks.

--- app.js
+++ app.js
@@ -1,27 +1,72 @@
-const textBox = document.getElementById
-('textbox');
-const taskName = document.getElementById
-('task-name');
-const categoryInput = document.getElementById
-('category');
-const deadLineInput = document.getElementById
-('deadLine');
-const statusInput = document.getElementById
-('status');
-const addTaskBtn = document.getElementById
-('btn1');
 
-    const addTask = (event) => {
-        const taskText = task-name.value.trim
-    }
+
+// const textbox = document.getElementById('taskinput');
+// const button = document.getElementById('btn1');
+// const output = document.getElementById('output1');
+
+
+
+const dailyTasks = []
+// task [0]= "laundry"
+// task [1]= "Dishes"
+// task [2]= "Dusting"
+// task [3]= "Lawn care"
+console.log(dailyTasks);
+dailyTasks.push('laundry')
+dailyTasks.push('Dishes');
+dailyTasks.push('Dusting');
+dailyTasks.push('Lawn care');
+
+
+
+        // function display() {
+        //     output.innerHTML = textbox.value;
+        //     const formToReset = getElementById('output1');
+        //     formToReset.addEventListener('submit', (e) => {
+        //         e.preventDefault();
+        //         formToReset.reset();
+        //         output.innerHTML += "Submitted successfully."
+        //     })
+        // }
+
+        // btn1.addEventListener('click',display);
+
+
+
+
+
+
+
+
+    // const addTask = (event) => {
+    //     event.preventDefault();
+    //     const taskText = taskInput.value.trim();
+    //     if(!taskText) {
+    //         return;
+    //     }
+    //     const li = document.createElement('li');
+    //     li.textContent = taskText;
+    //     taskList.appendChild(li);
+    //     taskname.value = '';
+    // };
+
+    // addTask.addEventListener('click', addTask);
+    // taskInput.addEventListener('keypress',(e) => {
+    //     if(e.key === 'Enter') {
+    //         addTask(e);
+    //     }
+    // });
 
  
-        
+// const taskInput = document.getElementById
+// ('taskinput');
+
+// const addTaskBtn = document.getElementById
+// ('btn1');
+
+// const taskList = document.getElementById('task-list');
 
 
-// const addTask = (event) => {
-//     const taskText = textbox.value.trim();
-// }
 
 
 
