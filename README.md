b433a2291543239f5c9e4dc0f3f88ffe6e487e2e
ShymeetriaJ <sjeducates2@gmail.com>
2025-10-29 23:33:28 -0400

    completed the README file.

---
+++ README.md
@@ -0,0 +1,20 @@
+Task Management App SBA Challenge
+
+Reflection Prompt and Response below.
+
+Write a short reflection (100-200 words) included within the repository discussing:
+
+Challenges faced during the project.
+How you approached solving those challenges.
+What you would improve if given more time.
+
+As I reflect on this challenge and this module, it's very evident that I need a deeper understanding of the javascript content. I approached the challenge by following the directions. First, I mapped out the html and that was the easiest task. I then attempted to apply the javascript. This was the most difficult part for me. Some things went well and some things didn't, such as adding on to arrays. I would improve my javascript codes if I had more time and play around more with the styling in css.
+
+Task Management App Challenge Instructions
+
+In this assessment, you will build a Task Management App that allows users to add tasks with deadlines, assign categories, and update the status of each task. This app will require you to apply a wide range of JavaScript concepts, including arrays, objects, DOM manipulation, conditionals, and local storage to persist the task data.
+
+This application is dynamic by design. Users are able to input tasks on the screen and describe the features of the task. Users also are able to track the status of their tasks by indicating if the task is in progress or completed. Users also can indicate the deadline for the task and remove tasks from the task list if they choose to. This application also features a filter dropdown menu where users can filter by category or status.
+
+
+
--- app.js
+++ app.js
@@ -1,35 +1,14 @@
 
 
-// const textbox = document.getElementById('taskinput');
-// const button = document.getElementById('btn1');
-// const output = document.getElementById('output1');
-
-
-
 const dailyTasks = []
-// task [0]= "laundry"
-// task [1]= "Dishes"
-// task [2]= "Dusting"
-// task [3]= "Lawn care"
 console.log(dailyTasks);
 dailyTasks.push('laundry')
 dailyTasks.push('Dishes');
 dailyTasks.push('Dusting');
 dailyTasks.push('Lawn care');
 
-
-
-        // function display() {
-        //     output.innerHTML = textbox.value;
-        //     const formToReset = getElementById('output1');
-        //     formToReset.addEventListener('submit', (e) => {
-        //         e.preventDefault();
-        //         formToReset.reset();
-        //         output.innerHTML += "Submitted successfully."
-        //     })
-        // }
-
-        // btn1.addEventListener('click',display);
+dailyTasks[0].laundry.push('name: "laundry", category: "", deadline: "", status: ""')
+console.log(dailyTasks);
 
 
 
@@ -38,33 +17,62 @@ dailyTasks.push('Lawn care');
 
 
 
-    // const addTask = (event) => {
-    //     event.preventDefault();
-    //     const taskText = taskInput.value.trim();
-    //     if(!taskText) {
-    //         return;
-    //     }
-    //     const li = document.createElement('li');
-    //     li.textContent = taskText;
-    //     taskList.appendChild(li);
-    //     taskname.value = '';
-    // };
 
-    // addTask.addEventListener('click', addTask);
-    // taskInput.addEventListener('keypress',(e) => {
-    //     if(e.key === 'Enter') {
-    //         addTask(e);
-    //     }
-    // });
+
+const textbox = document.getElementById('taskinput');
+const button = document.getElementById('btn1');
+const output = document.getElementById('output1');
+
+
+
+
+
+
+
+
+
+        function display() {
+            output.innerHTML = textbox.value;
+            const formToReset = getElementById('output1');
+            formToReset.addEventListener('submit', (e) => {
+                e.preventDefault();
+                formToReset.reset();
+                output.innerHTML += "Submitted successfully."
+            })
+        }
+
+        btn1.addEventListener('click',display);
+
+
+
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if(!taskText) {
+            return;
+        }
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskname.value = '';
+    };
+
+    addTask.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress',(e) => {
+        if(e.key === 'Enter') {
+            addTask(e);
+        }
+    });
 
  
-// const taskInput = document.getElementById
-// ('taskinput');
+const taskInput = document.getElementById
+('taskinput');
 
-// const addTaskBtn = document.getElementById
-// ('btn1');
+const addTaskBtn = document.getElementById
+('btn1');
 
-// const taskList = document.getElementById('task-list');
+const taskList = document.getElementById('task-list');
 
 
 
